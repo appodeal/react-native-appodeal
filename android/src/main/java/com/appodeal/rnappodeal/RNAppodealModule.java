0abce7087d4fa6d46ec2c7ca339a174873e177f9
Denis Glavatskikh <denis.glavatskikh@appodeal.com>
2024-04-02 19:57:44 +0200

    release/3.3.0-beta.3: updated Android part;

--- android/src/main/java/com/appodeal/rnappodeal/RNAppodealModule.java
+++ android/src/main/java/com/appodeal/rnappodeal/RNAppodealModule.java
@@ -56,7 +56,7 @@ public class RNAppodealModule extends ReactContextBaseJavaModule implements Inte
     }
 
     private String getPluginVersion() {
-        return "3.3.0-beta.2";
+        return "3.3.0-beta.3";
     }
 
     private void sendEventToJS(String eventName, WritableMap params) {
