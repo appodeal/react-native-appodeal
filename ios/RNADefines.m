e19ac7b83d813ecbba2154a0523d5acde467aa99
Evgenia Gorbacheva <evgenia.gorbacheva@appodeal.com>
2024-04-02 14:03:21 +0200

    Updated RNAVersion

--- ios/RNADefines.m
+++ ios/RNADefines.m
@@ -63,7 +63,7 @@ RCT_ENUM_CONVERTER(AppodealUserGender, (@{
 #pragma mark - Utils
 
 NSString *RNAVersion(void) {
-    return @"3.3.0-beta.2";
+    return @"3.3.0-beta.3";
 }
 
 NSArray<NSString *> *RNASupportedMehtods(void) {
